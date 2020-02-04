492b7c992f56f3bdac412f9ba50fa4965478fec9
zx122248006 <zx122248006@qq.com>
2020-02-04 18:41:59 +0800

    修改js

--- js/new.js
+++ js/new.js
@@ -37,17 +37,21 @@
 
       this.defaults = {
         effect: 'slide',
-        imgWidth: '800px',
-        imgHeight: '500px',
-        showBtn: 'true'
+        imgWidth: '800',
+        imgHeight: '500',
+        showBtn: 'true',
+        clickTime: '500',
+        iColorName: 'on',
+        showIndex: 'true'
       }
+      // 将默认值和传递过来的选项合并
       this.options = $.extend({}, this.defaults, option)
 
 
     }
 
     GmaFocus () {
-      let $index = 0; //定义索引数
+      // var $index = 0; //定义索引数
       let $this = this.elemnet //便于调用元素
       let $options = this.options //便于调用options
 
@@ -56,79 +60,85 @@
       let $ulwidth = $liNum * $imgWidth//设置ul的宽度为li的数量*li的宽度
 
 
+      // 测试
       let test = {
         b: {
           liNum: $liNum,
           imgWidth: $imgWidth,
-          ulwidth: $ulwidth
+          ulwidth: $ulwidth,
+          $index: 0
         }
       }
 
-
-
-      console.log($options)
-
+      // 将测试选项合并到之前的设置中
       $options = $.extend({}, $options, test)
 
       console.log($options)
 
+      // 调用设置样式的方法
       setStyle($this, $options, $ulwidth)
-      setBtn($this, $options)
+
+      // 调用左右切换的方法
+      btnClick($this, $options)
+
+      if ($options.showIndex) {
+        setBtn($this, $options)
+      }
 
     }
   }
 
-
   //根据li宽高设置ul的宽度
-  function setStyle (element, options, ulwidth) {
+  function setStyle (ele, options, ulwidth) {
 
     // 设置共同的样式
 
     // 根据图片一张图片宽度设置展示页面框的大小
-    element.Gma_wrap.css('width', options.imgWidth)
+    ele.Gma_wrap.css('width', options.imgWidth)
 
     // 设置图片及其容器的大小（由于图片宽高随li改变，所以只设置li的宽高）
-    element.innerLi.css({
-      'width': options.imgWidth + 'px',
+    ele.innerLi.css({
+      'width': options.imgWidth,
       'height': options.imgHeight
     })
 
     if (options.effect == 'slide') {   // 当切换效果为slide时，设置的样式
-
       //根据图片数量设置ul的宽度
-      element.centerbox.css('width', ulwidth)
+      ele.centerbox.css('width', ulwidth)
 
     } else if (options.effect == 'fade') { //当切换效果为fade时，设置的样式
 
-      element.Gma_wrap.css({
+      ele.Gma_wrap.css({
         'position': 'relative'
       });
 
       //与slide不同，fade效果下centerbox的宽度与li的宽高应该相同。
-      element.centerbox.css({
+      ele.centerbox.css({
         'width': options.imgWidth,
         'height': options.imgHeight
       })
 
-      element.innerLi.css({
+      ele.innerLi.css({
         'position': 'absolute',
         'display': 'none'
       })
 
-      element.innerLi.eq(0).fadeIn();
+      ele.innerLi.eq(0).fadeIn();
     }
 
   }
 
   // 判断是否显示切换按钮，及点击切换按钮
-  function setBtn (element, options) {
+  function btnClick (ele, opt) {
     let i = 0;
 
-    let $prev = element.Gma_wrap.find('.prev'); //在动态增加切换按钮之后获取切换按钮
-    let $next = element.Gma_wrap.find('.next');
 
-    if (options.showBtn) {
-      element.Gma_wrap.hover(
+
+    let $prev = ele.Gma_wrap.find('.prev'); //在动态增加切换按钮之后获取切换按钮
+    let $next = ele.Gma_wrap.find('.next');
+
+    if (opt.showBtn) {
+      ele.Gma_wrap.hover(
         function () {
           btnHover($prev, $next, 0.8)
         },
@@ -138,17 +148,17 @@
       )
 
       $next.click(function () {
-        let $liNum = element.innerLi.length //获取li的数量
-        if (!element.centerbox.is(':animated')) {
-          i++;
-          if (i > $liNum - 1) {
-            i = 0;
-            changeto(i, element, options);
+
+        let $liNum = ele.innerLi.length //获取li的数量
+        if (!ele.centerbox.is(':animated')) {
+          opt.b.$index++;
+          if (opt.b.$index > $liNum - 1) {
+            opt.b.$index = 0;
+            changeto(opt.b.$index, ele, opt);
           } else {
-            changeto(i, element, options);
+            changeto(opt.b.$index, ele, opt);
           }
         }
-        console.log('1')
       })
 
       $prev.click(function () {
@@ -161,7 +171,6 @@
         //     changeto(i);
         //   }
         // }
-        console.log('2')
       })
     }
   }
@@ -176,12 +185,61 @@
     })
   }
 
+  function changeto (num, ele, opt) {
+    // let $fbtn = ele.innerLi; // 获取切换索引
+    let $go = num * opt.imgWidth; // 设置移动的值
+
+    let $fbtn = ele.Gma_wrap.find('.fbtn ul li');
+
+    if (opt.effect == 'slide') { //判断切换的效果为slide时
+
+      ele.centerbox.animate({
+        left: '-' + $go
+      }, opt.clickTime);
+
+    } else if (opt.effect == 'fade') { //判断切换效果为fade时
+      ele.innerLi.eq(num).fadeIn().siblings().fadeOut()
+    }
+    $fbtn.removeClass(opt.iColorName).eq(num).addClass(opt.iColorName);
+  }
+
+  function setBtn (ele, opt) {
 
 
+
+
+    // 显示索引，动态增加索引栏目
+    ele.Gma_wrap.append('<div class="fbtn"></div>').find('.fbtn').append('<ul></ul>');
+
+    $liNum = ele.innerLi.length;
+    for (let i = 0; i < $liNum; i++) {
+      if (i == 0) {
+        ele.Gma_wrap.find('.fbtn ul').append('<li class="' + opt.iColorName + '"><a href="javascript:void(0)"></a></li>');
+      } else {
+        ele.Gma_wrap.find('.fbtn ul').append('<li><a href="javascript:void(0)"></a></li>');
+      }
+    }
+
+    let $fbtn = ele.Gma_wrap.find('.fbtn ul li');
+
+    // 当鼠标在切换索引时的效果
+    $fbtn.on('click', function () {
+      let index = $(this).index();
+      changeto(index, ele, opt);
+      $fbtn.removeClass(opt.iColorName).eq(index).addClass(opt.iColorName);
+
+
+      console.log(opt.b.$index)
+      // 返回改变的i值
+      return opt.b.$index = index;
+    });
+
+  }
 
   $.fn.GmaFocus = function (options) {
     // let Focus = new Gma_Focus(this, options);
 
+    // 通过实例化，将页面中的预置参数传递到类中
     let Focus = new Basic(this, options);
 
     Focus.GmaFocus();
